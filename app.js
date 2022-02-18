fe82d97cdda9f4fa16c190cd6b2bd49ea4d7f215
artemisIsNotBeam <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-02-18 06:38:01 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+import { animals } from './animals';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = <h1>Click an animal for a fun fact</h1>;
+const animalFacts = <h1> very good title</h1>
+
+ReactDOM.render(animalFacts, document.getElementById('root'));
