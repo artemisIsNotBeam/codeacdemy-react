f65905659d54757815ada529004cf5616b7f9cd6
artemisIsNotBeam <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-02-18 07:16:37 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ import { animals } from './animals';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+const background = <img class="background" alt="ocean" src="/images/ocean.jpg"/>
 const title = <h1>Click an animal for a fun fact</h1>;
-const animalFacts = <h1> very good title</h1>
+const images = []
 
-ReactDOM.render(animalFacts, document.getElementById('root'));
+for (let i=0;i<animals.length;i++){
+  images.push(<img src={animals[i].image}/>);
+}
+
+
+const animalFacts = (
+  <div>
+    {title}
+    {images}
+  </div>
+)
+
+const test = animals[1].image
+ReactDOM.render(images, document.getElementById('root'));
